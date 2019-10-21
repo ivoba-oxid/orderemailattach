b91cf6d9d01212ca0b7debafb11ce316cf144e96
Ivo Bathke <ivo.bathke@gmail.com>
2019-10-21 10:02:43 +0200

    init

---
+++ views/admin/de/module_options.php
@@ -0,0 +1,7 @@
+<?php
+$aLang = [
+    'charset'                                           => 'UTF-8',
+    'SHOP_MODULE_GROUP_ivoba_orderemailattach_main'     => 'Einstellungen',
+    'SHOP_MODULE_ivoba_orderemailattach_attachments_de' => 'Email Anhänge DE (in out/pdf)',
+    'SHOP_MODULE_ivoba_orderemailattach_attachments_en' => 'Email Anhänge EN (in out/pdf)',
+];
---
+++ views/admin/en/module_options.php
@@ -0,0 +1,7 @@
+<?php
+$aLang = [
+    'charset'                                           => 'UTF-8',
+    'SHOP_MODULE_GROUP_ivoba_orderemailattach_main'     => 'Settings',
+    'SHOP_MODULE_ivoba_orderemailattach_attachments_de' => 'Email attachments DE (in out/pdf)',
+    'SHOP_MODULE_ivoba_orderemailattach_attachments_en' => 'Email attachments EN (in out/pdf)',
+];
