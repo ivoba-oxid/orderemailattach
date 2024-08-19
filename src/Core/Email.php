ca048680ec16b1904e931f114c9da763020d6412
Ivo Bathke <ivo.bathke@gmail.com>
2024-08-19 09:14:07 +0200

    fixed config retrival

--- src/Core/Email.php
+++ src/Core/Email.php
@@ -2,7 +2,11 @@
 
 namespace IvobaOxid\OrderEmailAttach\Core;
 
+use IvobaOxid\OrderEmailAttach\Module;
 use OxidEsales\EshopCommunity\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Bridge\ModuleSettingBridgeInterface;
+
 
 class Email extends Email_parent
 {
@@ -17,7 +21,7 @@ class Email extends Email_parent
      * Returns true on success.
      *
      * @param \OxidEsales\Eshop\Application\Model\Order $order Order object
-     * @param string $subject                                  user defined subject [optional]
+     * @param string $subject user defined subject [optional]
      *
      * @return bool
      * @throws \Exception
@@ -27,7 +31,7 @@ class Email extends Email_parent
         $this->_attachmentsClear = false;
         $this->addOrderEmailAttachments();
 
-        $return                  = parent::sendOrderEmailToUser($order, $subject);
+        $return = parent::sendOrderEmailToUser($order, $subject);
 
         $this->_attachmentsClear = true;
 
@@ -39,19 +43,17 @@ class Email extends Email_parent
      * Returns true on success.
      *
      * @param \OxidEsales\Eshop\Application\Model\Order $order Order object
-     * @param string $subject                                  user defined subject [optional]
+     * @param string $subject user defined subject [optional]
      *
      * @return bool
      * @throws \Exception
      */
     public function sendOrderEmailToOwner($order, $subject = null)
     {
-        $cfg                     = Registry::getConfig();
-        if ($cfg->getConfigParam('ivoba_orderemailattach_add_to_ownermail')) {
+        if ($this->getConfigParam('ivoba_orderemailattach_add_to_ownermail')) {
             $this->_attachmentsClear = false;
             $this->addOrderEmailAttachments();
-        }
-        else {
+        } else {
             $this->clearAttachments();
         }
 
@@ -67,22 +69,22 @@ class Email extends Email_parent
      */
     protected function addOrderEmailAttachments()
     {
-        $path    = __DIR__.'/../../../../out/pdf/'; //todo hardcoded, should be uploadable
+        $path = __DIR__ . '/../../../../out/pdf/'; //todo hardcoded, should be uploadable
         $attPath = Registry::getUtilsFile()->normalizeDir($path);
 
         //detect language and add for language
-        $language      = \OxidEsales\Eshop\Core\Registry::getLang();
+        $language = \OxidEsales\Eshop\Core\Registry::getLang();
         $orderLanguage = $language->getLanguageAbbr($language->getTplLanguage());
-        $files         = explode(
+        $files = explode(
             ',',
-            $this->getConfig()->getConfigParam('ivoba_orderemailattach_attachments_'.$orderLanguage)
+            $this->getConfigParam('ivoba_orderemailattach_attachments_' . $orderLanguage)
         );
         foreach ($files as $attFile) {
-            $fullPath = $attPath.trim($attFile);
+            $fullPath = $attPath . trim($attFile);
             if (@is_readable($fullPath) && @is_file($fullPath)) {
                 $this->addAttachment($fullPath, $attFile);
             } else {
-                throw new \Exception('Attachment not found: '.$fullPath);
+                throw new \Exception('Attachment not found: ' . $fullPath);
             }
         }
     }
@@ -93,9 +95,15 @@ class Email extends Email_parent
     public function clearAttachments()
     {
         if ($this->_attachmentsClear === true) {
-            return parent::clearAttachments();
+            parent::clearAttachments();
         }
+    }
 
-        return;
+    private function getConfigParam(string $name)
+    {
+        $moduleSettingBridge = ContainerFactory::getInstance()
+            ->getContainer()
+            ->get(ModuleSettingBridgeInterface::class);
+        return $moduleSettingBridge->get($name, Module::MODULE_ID);
     }
 }
